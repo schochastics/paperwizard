c85b7a0f79a7cda98f9288b86629591392dce3be
schochastics <david@schochastics.net>
2024-10-26 20:58:01 +0200

    added more feeds [no ci]

--- data-raw/feeds.R
+++ data-raw/feeds.R
@@ -1,18 +1,36 @@
-url <- "https://rss.feedspot.com/world_news_rss_feeds/"
-doc <- rvest::read_html(url)
+url <- c(
+    "https://rss.feedspot.com/world_news_rss_feeds/",
+    "https://rss.feedspot.com/german_news_rss_feeds/",
+    "https://rss.feedspot.com/uk_news_rss_feeds/",
+    "https://rss.feedspot.com/usa_news_rss_feeds/",
+    "https://rss.feedspot.com/french_news_rss_feeds/",
+    "https://rss.feedspot.com/italian_news_rss_feeds/",
+    "https://rss.feedspot.com/european_news_rss_feeds/"
+)
 
-feeds <- tibble::tibble(
-    name = doc |>
-        rvest::html_elements("h3 a") |>
-        rvest::html_text(),
-    link = doc |>
-        rvest::html_elements(".trow.trow-wrap") |>
-        rvest::html_elements("a.ext:not(.extdomain)") |>
-        rvest::html_attr("href")
-) |>
-    dplyr::filter(link != "")
+get_links <- function(x) {
+    doc <- rvest::read_html(x)
 
-res <- paperboy::pb_collect(feeds$link, ignore_fails = TRUE)
-res <- res |> dplyr::filter(status == 200)
-articles <- pw_deliver(res)
+    feeds <- tibble::tibble(
+        name = doc |>
+            rvest::html_elements("h3 a") |>
+            rvest::html_text(),
+        link = doc |>
+            rvest::html_elements(".trow.trow-wrap") |>
+            rvest::html_elements("a.ext:not(.extdomain)") |>
+            rvest::html_attr("href")
+    ) |>
+        dplyr::filter(link != "")
+    return(feeds)
+}
+
+feeds <- purrr::map_dfr(url, get_links)
+
+feeds <- feeds |> dplyr::distinct(link, .keep_all = TRUE)
+
+res <- vector("list", nrow(feeds))
+for (i in seq_len(nrow(feeds))) res[[i]] <- tryCatch(paperboy::pb_collect(feeds$link[i], ignore_fails = TRUE), error = function(e) NULL)
+
+res_tbl <- do.call("rbind", res) |> dplyr::filter(status == 200)
+articles <- pw_deliver(res_tbl)
 pw_report(articles)
